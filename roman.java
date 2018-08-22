26b3c7bb787b62f17d14066e444635127ce56d0a
janani1998 <41136718+janani1998@users.noreply.github.com>
2018-08-22 10:34:03 +0530

    Create roman.java

---
+++ roman.java
@@ -0,0 +1,21 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	   Scanner in=new Scanner(System.in);
+	   String s=in.next();
+	   String a[]={"I","II","III","IV","V","VI","VII","VIII","IX","X","XI","XII","XIII","XIV","XV","XVI","XVII","XVIII","XIX","XX"};
+	   int l=a.length;
+	   for(int i=0;i<l;i++){
+	   	if(a[i].equals(s))
+	   	System.out.print(i+1);
+	   }
+	}
+}
